e7e33560f367f08593e6030820eeceeadabc823b
AlcatraZ-1337 <74929683+AlcatraZ-1337@users.noreply.github.com>
2021-04-16 21:07:29 +0300

    Начало создания станций

---
+++ stations.py
@@ -0,0 +1,14 @@
+def novocherkasskaya(update, context):
+    return 7
+
+
+def alexander_nevsky_square_1(update, context):
+    pass
+
+
+def alexander_nevsky_square_2(update, context):
+    pass
+
+
+def mayakovskaya(update, context):
+    pass
